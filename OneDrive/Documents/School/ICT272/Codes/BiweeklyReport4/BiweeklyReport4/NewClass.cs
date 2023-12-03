dd0e1e7001915aef2369ee49b705113474d2d018
Regine <rdelmo172@gmail.com>
2023-12-04 10:37:13 +1100

    adding a new class

---
+++ OneDrive/Documents/School/ICT272/Codes/BiweeklyReport4/BiweeklyReport4/NewClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiweeklyReport4
+{
+    class NewClass
+    {
+        //This is a new class.
+    }
+}
